ab4ed9423c0a63c52372ef7c6f53cf462919dac1
Fabien Chaix <chaix@ics.forth.gr>
2023-05-16 14:15:17 +0300

    Add a template C++ bench use case

---
+++ src/usecases/llbench.cpp
@@ -0,0 +1,14 @@
+
+#include <boost/program_options.hpp>
+#include <pthread.h>
+#include "c2c.h"
+
+
+namespace po = boost::program_options;
+
+
+int main(int argc, char *argv[])
+{	
+	
+    return 0;
+}
